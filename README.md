f605bf66ec946b74084a90db8c9b0114222b982b
codeguys <codeguys@demo.jasontech>
2022-08-08 13:02:35 +0800

    add template

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# django-playground
+# demo site
 
 ## Setup
 
--- src/jasontech/mainsite/views.py
+++ src/jasontech/mainsite/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    msg = 'Hello'
+    now = datetime.now()
+    hour = now.timetuple().tm_hour
+    return render(request, 'mainsite/index.html', locals())
