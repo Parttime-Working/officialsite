018b6b9e3dee9e4d8e22cd8ef345586390b90744
codeguys <codeguys@demo.jasontech>
2022-08-09 13:50:07 +0800

    chore: add djlint & move url to proj

---
+++ src/jasontech/mainsite/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'mainsite'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('ministry/', views.ministry, name='ministry'),
+    path('sermons/', views.sermons, name='sermons'),
+    path('events/', views.events, name='events'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+]
